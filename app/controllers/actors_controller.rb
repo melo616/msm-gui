7a5aae1c4a4ac848a9a39b35218eb37248bc8e14
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-02 20:25:17 +0000

    added update actor form and logic

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -28,10 +28,23 @@ class ActorsController < ApplicationController
   end
 
   def update
+    the_actor = Actor.where( { :id => params.fetch("path_id") }).at(0)
 
+    the_actor.name = params.fetch("name")
+    the_actor.dob = params.fetch("dob")
+    the_actor.bio = params.fetch("bio")
+    the_actor.image = params.fetch("image")
+
+    the_actor.save
+
+    redirect_to("/actors/#{ the_actor.id }")
   end
 
   def destroy
-    
+    the_actor = Actor.where( {:id => params.fetch("path_id") }).at(0)
+
+    the_actor.destroy
+
+    redirect_to("/actors")
   end
 end
